2c322d371a993a8b594784c392ac88ef0c473a7d
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-04-05 22:03:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", "20807000"))
     API_HASH = os.environ.get("API_HASH", "cde2366a7c61e23f4cb44618cbe6cf70")
     AUTH_USERS = [890749443]   # 👈 sirf apna ID
+    AUTH_USERS = [int(x) for x in AUTH_USER if x]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = "〖ᴷⁱⁿᴳ Ⱥʂʂìցղҽɾ 🔰❌️🔰♡▪︎•Å𝑁𝐈𐌑À𝐿•▪︎♡ ⱽᵉʳⁱᶠⁱᵉᵈ"#Here You Can Change with Your Name  or any custom name or title you prefer
